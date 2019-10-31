63779035bf7348e3ac357734fb8671a498189e7e
Patrick Gooch <goocpat@americas.manulife.net>
2019-10-31 11:09:20 -0400

    Chapter 11: adding API

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,6 +18,15 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', (req, res) => {
+  // Hard coded for testing
+  res.json([
+    { "id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@sample.org" },
+    { "id": 2, "firstName": "Tammy", "lastName": "Norton", "email": "tnorton@sample.org" },
+    { "id": 3, "firstName": "Tina", "lastName": "Lee", "email": "tinal@sample.org" },
+  ]);
+});
+
 app.listen(port, (err) => {
   if (err) {
     console.log(err);
