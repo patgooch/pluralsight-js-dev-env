c24956a951617705fada042eab0bc6600d499de8
Patrick Gooch <goocpat@americas.manulife.net>
2019-10-31 15:47:12 -0400

    Chapter 11: Mocked

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,11 +1,14 @@
 import 'whatwg-fetch';
+import getBaseUrl from './baseUrl';
+
+const baseUrl = getBaseUrl();
 
 export function getUsers() {
   return get('users');
 }
 
 function get(url) {
-  return fetch(url).then(onSuccess, onError);
+  return fetch(baseUrl + url).then(onSuccess, onError);
 }
 
 function onSuccess(response) {
@@ -14,4 +17,16 @@ function onSuccess(response) {
 
 function onError(error) {
   console.log(error); // eslint-disable-line no-console
+}
+
+export function deleteUser(id) {
+  return del(`users/${id}`);
+}
+
+function del(url) {
+  const request = new Request(baseUrl + url, {
+    method: 'DELETE'
+  });
+
+  return fetch(request).then(onSuccess, onError);
 }
